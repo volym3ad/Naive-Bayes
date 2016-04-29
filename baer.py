01504362487423d222be5ce36efe5c663a49c802
Vladyslav Voloshyn <kalym.share@gmail.com>
2016-04-29 16:22:47 +0300

    Added files via upload

--- baer.py
+++ baer.py
@@ -1,4 +1,4 @@
-import csv
+import csv # csv is a special format for table records
 import random
 import math
 
@@ -9,38 +9,34 @@ def loadCsv(filename):
         dataset[i] = [float(x) for x in dataset[i]]
     return dataset
 
-filename = 'pima-indians-diabetes.data.csv'
-dataset = loadCsv(filename)
-print('Loaded data file {0} with {1} rows').format(filename, len(dataset))
-
 def splitDataset(dataset, splitRatio):
 	trainSize = int(len(dataset) * splitRatio)
 	trainSet = []
 	copy = list(dataset)
 	while len(trainSet) < trainSize:
-		index = random.randrange(len(copy))
-		trainSet.append(copy.pop(index))
+		index = random.randrange(len(copy)) # returns a randomly selected element from range
+		trainSet.append(copy.pop(index)) # pop() - returns a removed object from the list
 	return [trainSet, copy]
 
 def separateByClass(dataset):
 	separated = {}
 	for i in range(len(dataset)):
 		vector = dataset[i]
-		if (vector[-1] not in separated):
+		if (vector[-1] not in separated):   # last column checks whether patient suffered from diabetes
 			separated[vector[-1]] = []
 		separated[vector[-1]].append(vector)
 	return separated
 
 def mean(numbers):
-	return sum(numbers)/float(len(numbers))
+	return sum(numbers)/float(len(numbers)) # calculte the mean - middle value
 
 def stdev(numbers):
 	avg = mean(numbers)
 	variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
-	return math.sqrt(variance)
+	return math.sqrt(variance) # standard deviation
 
 def summarize(dataset):
-	summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
+	summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)] # create a list with mean and deviation values
 	del summaries[-1]
 	return summaries
 
@@ -48,15 +44,15 @@ def summarizeByClass(dataset):
 	separated = separateByClass(dataset)
 	summaries = {}
 	for classValue, instances in separated.iteritems():
-		summaries[classValue] = summarize(instances)
+		summaries[classValue] = summarize(instances) # divide summarize by Class values (0, 1)
 	return summaries
 
 def calculateProbability(x, mean, stdev):
 	exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
-	return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent   # за розподілом Гауса
+	return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent   # Gauss function
 
-def calculateClassProbabilities(summaries, inputVector):
-	probabilities = {}
+def calculateClassProbabilities(summaries, inputVector): # calculate gauss function for class by multiplying each gauss attribute
+	probabilities = {}                                   # and displays result for each class
 	for classValue, classSummaries in summaries.iteritems():
 		probabilities[classValue] = 1
 		for i in range(len(classSummaries)):
@@ -65,7 +61,7 @@ def calculateClassProbabilities(summaries, inputVector):
 			probabilities[classValue] *= calculateProbability(x, mean, stdev)
 	return probabilities
 
-def predict(summaries, inputVector):
+def predict(summaries, inputVector): # compare the values of class to decide to which class input belongs
 	probabilities = calculateClassProbabilities(summaries, inputVector)
 	bestLabel, bestProb = None, -1
 	for classValue, probability in probabilities.iteritems():
@@ -74,14 +70,14 @@ def predict(summaries, inputVector):
 			bestLabel = classValue
 	return bestLabel
 
-def getPredictions(summaries, testSet):
+def getPredictions(summaries, testSet): # get predictions by all instances
 	predictions = []
 	for i in range(len(testSet)):
 		result = predict(summaries, testSet[i])
 		predictions.append(result)
 	return predictions
 
-def getAccuracy(testSet, predictions):
+def getAccuracy(testSet, predictions): # check the difference between predict() and getPredictions()
 	correct = 0
 	for i in range(len(testSet)):
 		if testSet[i][-1] == predictions[i]:
@@ -92,14 +88,20 @@ def main():
 	filename = 'pima-indians-diabetes.data.csv'
 	splitRatio = 0.67
 	dataset = loadCsv(filename)
+	print('Loaded data file {0} with {1} rows').format(filename, len(dataset))
 	trainingSet, testSet = splitDataset(dataset, splitRatio)
-	print('Split {0} rows into train={1} and test={2} rows').format(len(dataset), len(trainingSet), len(testSet))
+	print('Split {0} rows into train = {1} and test = {2} rows').format(len(dataset), len(trainingSet), len(testSet))
 	# prepare model
 	summaries = summarizeByClass(trainingSet)
+	for iter, value in summaries.iteritems():
+		print("\nClass: {0} --------------------------------- Value:\n {1}").format(iter, value)
 	# test model
 	predictions = getPredictions(summaries, testSet)
+	print '\n'
+	print predictions
+	
 	accuracy = getAccuracy(testSet, predictions)
-	print('Accuracy: {0}%').format(accuracy)
+	print('\nAccuracy: {0} %').format(accuracy)
 
 if __name__ == "__main__":
     main()
